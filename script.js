295ed3aa414b6c7cad06823367c6d70add3ef0b8
ItzValen01 <gaderracrack@gmail.com>
2026-03-31 21:28:02 -0300

    Casi terminado.

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
    attribution: 'OpenStreetMap'
 }).addTo(map);
 
-L.marker([-34.6067, -58.3816])
-    .addTo(map)
+var elementos = L.featureGroup().addTo(map)
+
+var iconCustom = L.icon({
+    iconUrl: 'imagenes/image.png',
+    iconSize: [38, 38],
+    iconAnchor: [19, 38],
+    popupAncor: [0, -35]
+});
+
+L.marker([-34.6067, -58.3816], { icon: iconCustom })
+    .addTo(elementos)
     .bindPopup("Ubicacion inicial")
     .openPopup();
 
@@ -15,7 +24,11 @@ let puntos = [];
 map.on('click', function(e) {
     puntos.push([e.latlng.lat, e.latlng.lng]);
 
-    if (puntos.length === 2) {
-        L.polyline(puntos, { color: 'blue' }).addTo(map)
+    if (puntos.length === 5) {
+        L.polyline(puntos, { color: 'blue' }).addTo(elementos)
     }
+})
+
+document.getElementById("btnLimpiar").addEventListener('click', function(e) {
+    elementos.clearLayers();
 })
